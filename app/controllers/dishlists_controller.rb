34d8ee508326b0a3bb2362227444beef0f16bddd
Charles Crane <charlie.crane46@gmail.com>
2020-11-24 15:22:12 +0000

    Add create method

--- app/controllers/dishlists_controller.rb
+++ app/controllers/dishlists_controller.rb
@@ -1,5 +1,6 @@
 class DishlistsController < ApplicationController
   before_action :set_user, only: [:index, :create]
+
   def index
     @dishlists = @user.dishlists.all
   end
@@ -8,6 +9,13 @@ class DishlistsController < ApplicationController
   end
 
   def create
+    @dishlist = Dishlist.new(dishlist_params)
+    @dishlist.user = current_user
+    if @dishlist.save
+      redirect_to dishlist_path(@dishlist.id)
+    else
+      render :index
+    end
   end
 
   def update
@@ -16,7 +24,17 @@ class DishlistsController < ApplicationController
   def destroy
   end
 
+  private
+
   def set_user
     @user = current_user
+  end
+
+  def set_dishlist
+    @dishlist = Dishlist.find(params[:id])
+  end
+
+  def dishlist_params
+    params.require(:dishlist).permit(:name)
   end
 end
