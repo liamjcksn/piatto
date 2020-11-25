71f119dcb61679f8e12de68665fc7475c41ee72b
Lloyd Chapple <lloydchaps@gmail.com>
2020-11-25 14:25:53 +0000

    add dishes to dishlist from dishes/show.html

--- app/controllers/dishlist_dishes_controller.rb
+++ app/controllers/dishlist_dishes_controller.rb
@@ -1,5 +1,11 @@
 class DishlistDishesController < ApplicationController
   def create
+    @dishlist_dish = DishlistDish.create(strong_params)
+    if @dishlist_dish.save!
+      redirect_to user_dishlist_path(current_user, @dishlist_dish.dishlist)
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
@@ -10,5 +16,9 @@ class DishlistDishesController < ApplicationController
   private
 
   def set_dishlist
+  end
+
+  def strong_params
+    params.require(:dishlist_dish).permit(:dishlist_id, :dish_id)
   end
 end
