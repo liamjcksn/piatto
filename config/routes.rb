a20e5b55aac21c0876f4dc989b3fd6e441975aca
Charles Crane <charlie.crane46@gmail.com>
2020-11-26 14:17:22 +0000

    Restrict dishlist actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   get '/discover', to: 'pages#discover', as: 'discover'
   get '/profile/:id', to: 'pages#profile', as: 'profile'
   get '/search', to: 'pages#search', as: 'search'
- 
+
   devise_for :users, path: 'accounts'
   resources :users do
     resources :dishlists, only: [ :index, :show, :create, :update, :destroy ]
   end
-  
+
   resources :dishlist_dishes, only: [ :create, :destroy ]
   resources :dishes
   resources :restaurants, only: [ :show ]
