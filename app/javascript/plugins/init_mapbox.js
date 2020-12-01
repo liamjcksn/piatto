d09aa9e6eb7729f7129a2c5546a2da7b9904d197
Lloyd Chapple <lloydchaps@gmail.com>
2020-12-01 17:00:16 +0000

    show similar dishes on map markers nearby

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,23 +3,26 @@ import mapboxgl from 'mapbox-gl';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-const fitMapToMarker = (map, marker) => {
+const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
-  bounds.extend([ marker.lng, marker.lat ]);
+  markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
   map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
 
-  if (mapElement) { // only build a map if there's a div#map to inject into
-    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+
+if (mapElement) { // only build a map if there's a div#map to inject into
+  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10'
+  });
+  const markers = JSON.parse(mapElement.dataset.marker);
+  markers.forEach((marker) => {
+    new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .addTo(map);
     });
-    const marker = JSON.parse(mapElement.dataset.marker);
-      new mapboxgl.Marker()
-        .setLngLat([marker.lng, marker.lat])
-        .addTo(map);
-    fitMapToMarker(map, marker);
+  fitMapToMarkers(map, markers);
   }
 };
 
